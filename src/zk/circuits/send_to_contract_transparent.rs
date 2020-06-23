950e113e5c4611963fdda9273f0d5a107e9c3875
Jules de Smit <julesdesmit@gmail.com>
2020-06-23 19:37:37 +0200

    Clean up 'Send To Contract Transparent' gadget
    This includes grouping all of the gadgets for the inputs
    together in one iteration, as well as for the outputs.
    Besides this, instead of taking in a rest value, which will be sent
    to the contract, it is automatically calculated by use of an
    accumulator variable, which tallies up all of the input values,
    and subtracts all of the output values. It is then passed to the balance
    gadget, at the end of the function.

--- src/zk/circuits/send_to_contract_transparent.rs
+++ src/zk/circuits/send_to_contract_transparent.rs
@@ -1,45 +1,53 @@
-use crate::{db, zk::gadgets, Transaction};
+use crate::{db, zk::gadgets, NoteVariant, Transaction, TransactionItem};
 
-use dusk_plonk::constraint_system::{Proof, StandardComposer};
+use dusk_plonk::constraint_system::StandardComposer;
+use dusk_plonk::proof_system::Proof;
 
 /// This gadget constructs the circuit for a "Send To Contract Transparent" transaction.
-pub fn send_to_contract_transparent_gadget(
-    composer: &mut StandardComposer,
-    tx: &Transaction,
-    v: u64,
-) {
-    // Merkle openings + preimage knowledge + nullifier
+pub fn send_to_contract_transparent_gadget(composer: &mut StandardComposer, tx: &Transaction) {
+    // Define an accumulator, which will hold the amount being sent to the contract.
+    let v: u64 = 0;
+
+    // Inputs
     let db = db::Db::default();
     tx.inputs().iter().for_each(|input| {
+        // Merkle openings + preimage knowledge + nullifier
         // TODO: get branch
         gadgets::merkle(composer, branch, input);
         gadgets::input_preimage(composer, input);
         gadgets::nullifier(composer, input);
-    });
 
-    // Secret key knowledge
-    // TODO: insert `ecc_gate` function from PLONK
+        // Secret key knowledge
+        // TODO: insert `ecc_gate` function from PLONK
 
-    // Commitment knowledge + range proof
-    tx.inputs().iter().for_each(|input| {
+        // If the contained note is obfuscated, also include statements about
+        // the commitment preimage, and a range proof.
         match input.note() {
-            NoteVariant::Obfuscated => {
+            NoteVariant::Obfuscated(_) => {
                 gadgets::commitment(composer, input);
                 gadgets::range(composer, input);
-            },
-            _ => {},
+            }
+            _ => {}
         }
-    }
 
+        // Tally up input value into `v`
+        v += input.value();
+    });
+
+    // Outputs
     tx.outputs().iter().for_each(|output| {
+        // Commitment preimage knowledge + range proof
         match output.note() {
-            NoteVariant::Obfuscated => {
+            NoteVariant::Obfuscated(_) => {
                 gadgets::commitment(composer, output);
                 gadgets::range(composer, output);
-            },
-            _ => {},
+            }
+            _ => {}
         }
-    }
+
+        // Subtract output value from `v`
+        v -= output.value();
+    });
 
     // Inputs - outputs = 0
     gadgets::balance(composer, tx, v);
