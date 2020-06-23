e7da25ae2793754e68b21143b8c45f02b22aed62
Luke Pearson <luke@dusk.network>
2020-06-23 19:34:06 +0200

    Insert send circuit gadget
    The send gadget, for the dusk contract circuits, has been built in this commit. This can be considered a mock function as there are one or two minor things yet to be implemented, like the secret key and posiedon branch.

--- src/zk/circuits/send.rs
+++ src/zk/circuits/send.rs
@@ -1,5 +1,41 @@
-use crate::Transaction;
+use crate::{zk::gadgets, Transaction};
 
 use dusk_plonk::constraint_system::{Proof, StandardComposer};
 
-fn send_circuit(composer: &mut StandardComposer, tx: &Transaction) -> Proof {}
+fn send_gadget(composer: &mut StandardComposer, tx: &Transaction) {
+    tx.inputs().iter().for_each(|tx_input|{
+        gadgets::merkle(composer, branch, tx_input);
+        gadget::nullifier(composer, tx_input);
+        gadget::preimage(composer, tx_input);
+        //gadget::secret_key();
+    });
+
+    // Commitment knowledge and range proof
+    // for inputs
+    tx.inputs().iter().for_each(|tx_input| {
+        match input.note() {
+            NoteVariant::Obfuscated => {
+                gadgets::commitment(composer, tx_input);
+                gadgets::range(composer, tx_input);
+            },
+            _ => {},
+        }
+    }
+
+    // Commitment knowledge and range proof
+    // for inputs
+    tx.outputs().iter().for_each(|tx_output| {
+        match output.note() {
+            NoteVariant::Obfuscated => {
+                gadgets::commitment(composer, tx_output);
+                gadgets::range(composer, tx_output);
+            },
+            _ => {},
+        }
+    }
+
+    // Inputs - outputs = 0
+    gadgets::balance(composer, tx, 0);
+
+
+}
